6c2830d41a435d4eecbe7432f8e9b03c8ac427fb
Wilhelmina Slater <43039504+Willhelmina@users.noreply.github.com>
2020-11-21 16:55:48 +0800

    Add files via upload

---
+++ Lesson 10/test.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+* function：SimpleXML读XML文件
+**/
+//XML标签配置
+$study = array();
+$xml = simplexml_load_file('test1.tmx');
+foreach($xml->children() as $period) {
+    $study[] = get_object_vars($period);//获取对象全部属性，返回数组
+}
+echo '<pre>';
+print_r($study);
+?>
